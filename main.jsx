17a507e9f4e6e1ef08f5e9a6a2e1abe9d6da61dd
neurovitaapp <neurovita.app@gmail.com>
2026-06-04 11:27:08 +0100

    import VitalDoctor from './VitalDoctor_SaaS.jsx'

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import VitalDoctor from '../VitalDoctor_SaaS.jsx'
+import VitalDoctor from './VitalDoctor_SaaS.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
