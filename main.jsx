f8a5cf1be6e31c686a6cc6ea14ad166e6ca58330
neurovitaapp <neurovita.app@gmail.com>
2026-06-05 17:42:33 +0100

    Update and rename main.jsx.txt to main.jsx

---
+++ main.jsx
@@ -0,0 +1,4 @@
+import{StrictMode}from'react'
+import{createRoot}from'react-dom/client'
+import VitalDoctor from'./VitalDoctor_SaaS.jsx'
+createRoot(document.getElementById('root')).render(<StrictMode><VitalDoctor/></StrictMode>)
